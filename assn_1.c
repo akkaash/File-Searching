918b2c24f163ffce398284299e3c7565c4718a05
Akkaash Goel <agoel3@ncsu.edu>
2014-01-08 17:10:51 -0500

    version 1

--- assn_1.c
+++ assn_1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+long mDiffTime(struct timeval end, struct timeval start){
+	long ret = 0L;
+	ret += (end.tv_sec - start.tv_sec) * 1000000L;
+	ret += (end.tv_usec - start.tv_usec);
+	
+	return ret;
+}
 
 int binarySearch(int *arr, int bottom, int top, int s){
 	if(top < bottom)
@@ -9,6 +18,22 @@ int binarySearch(int *arr, int bottom, int top, int s){
 	if(arr[mid] == s)	return mid;
 	else if (s > arr[mid])	return binarySearch(arr, mid+1, top, s);
 	else if (s < arr[mid])	return binarySearch(arr, bottom, mid-1, s);
+}
+
+int binarySearchOnDisk(FILE *ip, int bottom, int top, int s){
+	if(top < bottom)
+		return -1;
+	
+	int mid = (bottom + top)/2;
+	int x;
+	
+	fseek(ip, mid*sizeof(int), SEEK_SET);
+	fread(&x, sizeof(int), 1, ip);
+	
+	if(s == x)		return mid;
+	else if(s > x)		return binarySearchOnDisk(ip, mid+1, top, s);
+	else if(s < x)		return binarySearchOnDisk(ip, bottom, mid-1, s);
+	
 }
 
 int main(int argc, char **argv){
@@ -39,10 +64,8 @@ int main(int argc, char **argv){
 	}
 	
 	FILE *ip1, *ip2;
-	
-	ip1 = fopen(inFile1, "rb");
-	ip2 = fopen(inFile2, "rb");
-	
+	long timeDiff = 0;
+		
 	int i, N=0;
 	int k;
 	
@@ -67,26 +90,7 @@ int main(int argc, char **argv){
 	*
 	*
 	*/
-		//----reading K----------------
-		for(i=0;;i++){
-			fseek(ip1, i*sizeof(int), SEEK_SET);
-			fread(&k, sizeof(int), 1, ip1);
-			if(feof(ip1) == 0){
-				N++;
-			} else break;
-		}
-		K = (int *) malloc(N * sizeof(int));
-		for(i=0;;i++){
-			fseek(ip1, i*sizeof(int), SEEK_SET);
-			fread(&k, sizeof(int), 1, ip1);
-			if(feof(ip1) == 0){
-				K[i] = k;
-			} else break;
-		}
-		//-----------------------------
-		KSize = N;
-		
-		N=0;
+		ip2 = fopen(inFile2, "rb");		//OPENING SEEK.DB
 		//----reading S----------------
 		for(i=0;;i++){
 			fseek(ip2, i*sizeof(int), SEEK_SET);
@@ -106,12 +110,43 @@ int main(int argc, char **argv){
 		//-----------------------------
 		SSize = N;
 		
+		N=0;
+		
+		struct timeval t1;
+		gettimeofday(&t1, NULL);
+		
+		ip1 = fopen(inFile1, "rb");		//OPENING KEY.DB
+		//----reading K----------------
+		for(i=0;;i++){
+			fseek(ip1, i*sizeof(int), SEEK_SET);
+			fread(&k, sizeof(int), 1, ip1);
+			if(feof(ip1) == 0){
+				N++;
+			} else break;
+		}
+		K = (int *) malloc(N * sizeof(int));
+		for(i=0;;i++){
+			fseek(ip1, i*sizeof(int), SEEK_SET);
+			fread(&k, sizeof(int), 1, ip1);
+			if(feof(ip1) == 0){
+				K[i] = k;
+			} else break;
+		}
+		//-----------------------------
+		KSize = N;
+		
+		struct timeval t2;
+		gettimeofday(&t2, NULL);
+		timeDiff += mDiffTime(t2, t1);
 		
 		hit = (int *) malloc(SSize * sizeof(int));
 		int seek, j, pos;
 		
-		//-------- linear search -----------
+		//-------- in memory linear search -----------
 		if(mode == 1){
+			struct timeval t3;
+			gettimeofday(&t3, NULL);
+			
 			for(i=0; i<SSize; i++){
 				seek = S[i];
 				for(j=0; j<KSize; j++){
@@ -123,12 +158,19 @@ int main(int argc, char **argv){
 					}
 				}
 			}
+			
+			struct timeval t4;
+			gettimeofday(&t4, NULL);
+			timeDiff += mDiffTime(t4, t3);
 				
 		} 
 		//----------------------------------
 		
-		//------- binary search ------------
+		//------- in memory binary search ------------
 		else if(mode == 2){
+			struct timeval t3;
+			gettimeofday(&t3, NULL);
+			
 			for(i=0; i<SSize; i++){
 				seek = S[i];
 				pos = binarySearch(K, 0, KSize-1, seek);
@@ -138,10 +180,113 @@ int main(int argc, char **argv){
 					hit[i] = 1;
 				}
 			}
+			struct timeval t4;
+			gettimeofday(&t4, NULL);
+			timeDiff += mDiffTime(t4, t3);
+			
 		}
 		//----------------------------------
 		
 		
+	} else if (mode == 3 || mode == 4){
+	/*
+	*
+	*
+	*	ON DISK SEARCH
+	*
+	*
+	*/
+		ip2 = fopen(inFile2, "rb");		//OPENING SEEK.DB		
+		//----reading S----------------
+		for(i=0;;i++){
+			fseek(ip2, i*sizeof(int), SEEK_SET);
+			fread(&k, sizeof(int), 1, ip2);
+			if(feof(ip2) == 0){
+				N++;
+			} else break;
+		}
+		S=(int *) malloc(N * sizeof(int));
+		for(i=0;;i++){
+			fseek(ip2, i*sizeof(int), SEEK_SET);
+			fread(&k, sizeof(int), 1, ip2);
+			if(feof(ip2) == 0){
+				S[i] = k;
+			} else break;
+		}		
+		//-----------------------------
+		SSize = N;
+		
+		hit = (int *) malloc(SSize * sizeof(int));
+		int seek, j, pos;
+		int foundFlag = 0;
+
+		if (mode == 3) {
+			struct timeval t1;
+			gettimeofday(&t1, NULL);
+			
+			ip1 = fopen(inFile1, "rb");	//OPENING KEY.DB
+			
+			//--------- ON DISK LINEAR SEARCH ---------------------
+			
+			for(i=0;i<SSize;i++){
+				//printf("i=%d\n", i);
+				seek = S[i];
+/*				printf("seek=%d ", seek);*/
+				for(j=0;;j++){
+					fseek(ip1, j*sizeof(int), SEEK_SET);
+					fread(&k, sizeof(int), 1, ip1);
+					//printf("k=%d ", k);
+					if(feof(ip1) == 0){
+						if(seek == k){
+/*							printf(" found\n");*/
+							hit[i] = 1;
+							break;
+						} else{
+							hit[i] = 0;
+						}
+					} else{
+/*						printf("eof\n");*/
+						break;
+						
+					} 
+				}
+			}
+			//---------------------------------------------
+			struct timeval t2;
+			gettimeofday(&t2, NULL);
+			timeDiff += mDiffTime(t2, t1);
+			
+		} else if (mode == 4){
+			struct timeval t1;
+			gettimeofday(&t1, NULL);
+			
+			ip1 = fopen(inFile1, "rb");	//OPENING KEY.DB
+			
+			//--------- ON DISK BINARY SEARCH ---------------------
+			N = 0;
+			for(i=0;;i++){
+				fseek(ip1, i*sizeof(int), SEEK_SET);
+				fread(&k, sizeof(int), 1, ip1);
+				if(feof(ip1) == 0){
+					N++;
+				} else break;
+			}
+			KSize = N;
+			for(i=0;i<SSize;i++){
+				seek = S[i];
+				pos = binarySearchOnDisk(ip1, 0, KSize-1, seek);
+				if(pos == -1){
+					hit[i] = 0;
+				} else{
+					hit[i] = 1;
+				}
+			}
+			struct timeval t2;
+			gettimeofday(&t2, NULL);
+			timeDiff += mDiffTime(t2, t1);
+		}			
+			
+	
 	}
 	
 	
@@ -159,5 +304,7 @@ int main(int argc, char **argv){
 			printf( "%12d: No\n", S[i] );
 		}
 	}
+
+	printf("Time: %f\n", (float)timeDiff/1000000);
 	
 }
